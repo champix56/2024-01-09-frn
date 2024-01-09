994c33666fd4fea4e5d8df2c114a141d2db06966
champix56 <desorbaix@free.fr>
2024-01-09 15:32:42 +0100

    1er composant

---
+++ src/App/components/ui/Button/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, TouchableHighlight, View} from 'react-native';
+
+const Button = props => {
+  return (
+    <TouchableHighlight>
+      <View>
+        <Text>Benjamin</Text>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+export default Button;
