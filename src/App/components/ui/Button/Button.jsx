bfb18af54d1a4cd08d911aa126728f4ca86f8a39
champix56 <desorbaix@free.fr>
2024-01-09 16:19:05 +0100

    view props

--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Text, TouchableHighlight, View} from 'react-native';
 
 const Button = props => {
+  console.log(props);
   return (
     <TouchableHighlight>
       <View>
